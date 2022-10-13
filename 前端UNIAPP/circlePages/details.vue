3acc6175f646b7c4e46534d9d74cdd46bfa4f651
多客开源 <admin@siyibang.com>
2022-10-13 16:25:33 +0800

    no commit message

--- 前端UNIAPP/circlePages/details.vue
+++ 前端UNIAPP/circlePages/details.vue
@@ -76,9 +76,9 @@
 						<!-- 内容太多疲劳了-->
 						<view v-if="item.content" class="blogger__content" :id="`blogger__content--${item.id}`">
 							<view class="blogger__content__data">
-								<tn-read-more :closeBtn="true" :showHeight="300">
+								
 									<rich-text :nodes="item.content"></rich-text>
-								</tn-read-more>
+								
 							</view>
 
 						</view>
@@ -153,8 +153,8 @@
 
 					<!-- 按钮-->
 					<view class="tn-flex tn-flex-row-between" style="margin: 40rpx 0 60rpx 0;"
-					>
-						<view class="tn-flex-1 justify-content-item tn-margin-xs tn-text-center" @click="dianzantiezi(id)">
+						@click="dianzantiezi(id)">
+						<view class="tn-flex-1 justify-content-item tn-margin-xs tn-text-center">
 							<tn-button backgroundColor="#00FFC6" padding="40rpx 0" width="90%" shadow fontBold>
 								<text class="tn-icon-praise-fill tn-padding-right-xs "
 									:class="item.is_like?'':'tn-color-red'"></text>
